4695f504ed688be532b53e80a70d280557e2c4c6
Abhinav Gupta <abg@uber.com>
2019-07-08 13:20:09 -0700

    Implement main and end-to-end tests
    This implements the main.go for gopatch and sets up a basic end-to-end
    test framework.
    
    Note that the formatting in the test cases is all messed up. This is
    expected behavior until we handle token.Pos values properly, which will
    inform the Go formatter how we want the code formatted.
    
    Reviewed-on: D3035871

--- internal/goast/types.go
+++ internal/goast/types.go
@@ -2,13 +2,22 @@ package goast
 
 import (
 	"go/ast"
+	"go/token"
 	"reflect"
 )
 
 // Reflected types of various AST nodes.
 var (
+	// Primitives
+	PosType = reflect.TypeOf(token.Pos(0))
+
+	// Structs
+	IdentType  = reflect.TypeOf(ast.Ident{})
+	ObjectType = reflect.TypeOf(ast.Object{})
+
 	// Struct Pointers
-	IdentPtrType = reflect.TypeOf((*ast.Ident)(nil))
+	IdentPtrType  = reflect.PtrTo(IdentType)
+	ObjectPtrType = reflect.PtrTo(ObjectType)
 
 	// Interfaces
 	ExprType = reflect.TypeOf((*ast.Expr)(nil)).Elem()
