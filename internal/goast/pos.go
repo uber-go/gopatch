8636a1405a1091548fb2cd2f32f34187484d11c3
Abhinav Gupta <abg@uber.com>
2019-07-09 10:07:37 -0700

    goast/TransformPos: Handle File objects
    This changes TransformPos to handle File objects better. File objects
    are weird because unlike the rest of the AST node types, they maintain
    multiple references to the same value.
    
    In particular,
    
    - File.Scope maps names to ast.Object objects which contain references
      to the original declaration of an entity.
    - File.Imports contains references to all ImportSpecs found inside
      File.Decls.
    - File.Unresolved contains references to all ast.Ident objects found in
      the file that were unresolved (don't have an associated ast.Object).
    - File.Comments contains references to all comments found in the file.
      Nodes already reference their associated documentation with the .Doc
      field. The comments that remain here are free-floating comments.
      Because handling them complicates things to an unnecessary degree,
      we're not going to handle them.
    
    Reviewed-on: D3034423

--- internal/goast/pos.go
+++ internal/goast/pos.go
@@ -9,19 +9,26 @@ import (
 // TransformPos runs the given function on all token.Pos values found in the
 // given object and its descendants, replacing them in-place with the value
 // produced by the provided function.
+//
+// Caveat: Free-floating comments on File objects are not handled by
+// TransformPos.
 func TransformPos(n interface{}, transform func(token.Pos) token.Pos) {
 	transformPos(reflect.ValueOf(n), transform)
 }
 
 // OffsetPos offsets all token.Pos values found in the given object and its
 // descendants in-place.
+//
+// Caveat: Free-floating comments on File objects are not handled by
+// OffsetPos.
 func OffsetPos(n interface{}, offset int) {
 	TransformPos(n, func(pos token.Pos) token.Pos { return pos + token.Pos(offset) })
 }
 
 var (
-	posType    = reflect.TypeOf(token.Pos(0))
-	objectType = reflect.TypeOf((*ast.Object)(nil))
+	posType       = reflect.TypeOf(token.Pos(0))
+	objectPtrType = reflect.TypeOf((*ast.Object)(nil))
+	fileType      = reflect.TypeOf(ast.File{})
 )
 
 func transformPos(v reflect.Value, transformFn func(token.Pos) token.Pos) {
@@ -29,46 +36,60 @@ func transformPos(v reflect.Value, transformFn func(token.Pos) token.Pos) {
 		return
 	}
 
-	// token.Pos is present only as a struct field. Every other composite type
-	// can be recursed.
-	switch v.Kind() {
-	case reflect.Array, reflect.Slice:
-		for i := 0; i < v.Len(); i++ {
-			transformPos(v.Index(i), transformFn)
+	switch v.Type() {
+	case fileType:
+		// ast.File maintains a bunch of internal references. Only the
+		// following fields are unique references.
+		transformPos(v.FieldByName("Doc"), transformFn)
+		transformPos(v.FieldByName("Package"), transformFn)
+		transformPos(v.FieldByName("Name"), transformFn)
+		transformPos(v.FieldByName("Decls"), transformFn)
+
+		// NOTE: File.Comments contains both, comments that document
+		// objects (also referenced by those nodes' Doc fields), and
+		// free-floating comments in the file. Rather than tracking
+		// whether a comment has already been processed, we're just
+		// not going to handle free-floating comments until it becomes
+		// necessary.
+	case objectPtrType:
+		// ast.Object has a reference to the target object, causing
+		// cyclic references. Since the underlying object isn't
+		// changing, we don't need to do anything here.
+	case posType:
+		pos := token.Pos(v.Int())
+		if pos.IsValid() {
+			// We want to change Pos only if it's valid, as in
+			// non-zero. There are parts in the Go AST where the
+			// presence of a valid Pos changes the generated
+			// syntax significantly. One example is type aliases.
+			//
+			//   type Foo = Bar
+			//   type Foo Bar
+			//
+			// The only difference between the parsed
+			// representations for the two type declarations above
+			// is whether the Equals field has a valid token.Pos
+			// or not. If the Pos is invalid, we don't want to
+			// change it.
+			v.SetInt(int64(transformFn(pos)))
 		}
-	case reflect.Interface, reflect.Ptr:
-		transformPos(v.Elem(), transformFn)
-	case reflect.Struct:
-		for i := 0; i < v.NumField(); i++ {
-			f := v.Field(i)
-			switch f.Type() {
-			case objectType:
-				// ast.Object has a reference to the target object, causing
-				// cyclic references. Since the underlying object isn't
-				// changing, we don't need to do anything here.
-			case posType:
-				pos := token.Pos(f.Int())
-				if pos.IsValid() {
-					// We want to change Pos only if it's valid, as in
-					// non-zero. There are parts in the Go AST where the
-					// presence of a valid Pos changes the generated syntax
-					// significantly. One example is type aliases.
-					//
-					//   type Foo = Bar
-					//   type Foo Bar
-					//
-					// The only difference between the parsed representations
-					// for the two type declarations above is whether the
-					// Equals field has a valid token.Pos or not. If the Pos
-					// is invalid, we don't want to change it.
-					f.SetInt(int64(transformFn(pos)))
-				}
-			default:
-				transformPos(f, transformFn)
+	default:
+		switch v.Kind() {
+		case reflect.Array, reflect.Slice:
+			for i := 0; i < v.Len(); i++ {
+				transformPos(v.Index(i), transformFn)
 			}
+		case reflect.Interface, reflect.Ptr:
+			transformPos(v.Elem(), transformFn)
+		case reflect.Struct:
+			for i := 0; i < v.NumField(); i++ {
+				transformPos(v.Field(i), transformFn)
+			}
+		case reflect.Map:
+			// go/ast does not use maps in the AST besides Scope objects
+			// which are attached to File objects, which we handle
+			// explicitly.
+			panic("cannot use maps inside an AST node")
 		}
-	case reflect.Map:
-		// go/ast does not use maps in the AST. Neither do we.
-		panic("cannot use maps inside an AST node")
 	}
 }
--- internal/goast/pos_test.go
+++ internal/goast/pos_test.go
@@ -2,19 +2,25 @@ package goast
 
 import (
 	"go/ast"
+	"go/parser"
 	"go/token"
 	"testing"
 
+	"github.com/uber-go/gopatch/internal/text"
+	"github.com/kr/pretty"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestOffsetPos(t *testing.T) {
-	tests := []struct {
+	type testCase struct {
 		desc   string
 		give   interface{}
 		offset int
 		want   ast.Node
-	}{
+	}
+
+	tests := []testCase{
 		{
 			desc:   "identifier",
 			give:   &ast.Ident{NamePos: 42, Name: "Foo"},
@@ -57,12 +63,119 @@ func TestOffsetPos(t *testing.T) {
 				Rbrace: 30,
 			},
 		},
+		func() (tt testCase) {
+			tt.desc = "file with func"
+			tt.give = parseFile(t,
+				"package foo",
+				"func foo() {}",
+			)
+			tt.offset = 3
+
+			fooFuncObj := &ast.Object{
+				Kind: ast.Fun,
+				Name: "foo",
+			}
+
+			file := &ast.File{
+				Package: 4,
+				Name: &ast.Ident{
+					Name:    "foo",
+					NamePos: 12,
+				},
+				Decls: []ast.Decl{
+					&ast.FuncDecl{
+						Name: &ast.Ident{
+							Name:    "foo",
+							NamePos: 21,
+							Obj:     fooFuncObj,
+						},
+						Type: &ast.FuncType{
+							Func: 16,
+							Params: &ast.FieldList{
+								Opening: 24,
+								Closing: 25,
+							},
+						},
+						Body: &ast.BlockStmt{
+							Lbrace: 27,
+							Rbrace: 28,
+						},
+					},
+				},
+				Scope: &ast.Scope{
+					Objects: map[string]*ast.Object{
+						"foo": fooFuncObj,
+					},
+				},
+			}
+			tt.want = file
+			// Equivalent to,
+			//   parseFile(t, "   package foo", ...)
+			// (Ofsetting everything by 3.)
+
+			// Connect the cyclic reference.
+			fooFuncObj.Decl = file.Decls[0]
+
+			return
+		}(),
+		func() (tt testCase) {
+			tt.desc = "file with import"
+			tt.give = parseFile(t,
+				"package foo",
+				`import bar "bar.git"`,
+			)
+			tt.offset = 10
+
+			barImport := &ast.ImportSpec{
+				Name: &ast.Ident{
+					Name:    "bar",
+					NamePos: 30,
+				},
+				Path: &ast.BasicLit{
+					Value:    `"bar.git"`,
+					ValuePos: 34,
+					Kind:     token.STRING,
+				},
+			}
+
+			file := &ast.File{
+				Package: 11,
+				Name: &ast.Ident{
+					Name:    "foo",
+					NamePos: 19,
+				},
+				Decls: []ast.Decl{
+					&ast.GenDecl{
+						TokPos: 23,
+						Tok:    token.IMPORT,
+						Specs:  []ast.Spec{barImport},
+					},
+				},
+				// file.Imports contains references to all
+				// imports found in the file but the Decl is
+				// authoritative.
+				Imports: []*ast.ImportSpec{barImport},
+				Scope: &ast.Scope{
+					Objects: map[string]*ast.Object{},
+				},
+			}
+			tt.want = file
+			// Equivalent to,
+			//   parseFile(t, "          package foo", ...)
+			// (Offsetting everything by 10.)
+
+			return
+		}(),
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
 			OffsetPos(tt.give, tt.offset)
-			assert.Equal(t, tt.want, tt.give)
+			if !assert.Equal(t, tt.want, tt.give) {
+				// Handles cyclic references better than
+				// testify.
+				pretty.Ldiff(t, tt.want, tt.give)
+			}
 		})
 	}
 }
@@ -71,4 +184,11 @@ func TestOffsetPosPanicsOnMap(t *testing.T) {
 	assert.Panics(t, func() {
 		OffsetPos(struct{ M map[string]token.Pos }{}, 42)
 	})
+}
+
+func parseFile(t *testing.T, lines ...string) *ast.File {
+	src := text.Unlines(lines...)
+	file, err := parser.ParseFile(token.NewFileSet(), t.Name(), src, parser.ParseComments)
+	require.NoError(t, err, "failed to parse source")
+	return file
 }
